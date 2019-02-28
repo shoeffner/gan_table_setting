1e4d5e401821c9495880deca144385a6cf13a946
Sebastian Höffner <info@sebastian-hoeffner.de>
2019-02-28 18:29:19 +0100

    Restarting runner automatically.

--- runner.sh
+++ runner.sh
@@ -1,2 +1,8 @@
-#!/bin/bash -e
-PYTHONPATH=. hyperopt-mongo-worker --mongo=localhost:27017/sacred --poll-interval=5 --workdir=$(pwd)
+#!/bin/bash
+
+while true; do
+    PYTHONPATH=. hyperopt-mongo-worker --mongo=localhost:27017/sacred --workdir=$(pwd)
+    if [[ $? -ne 0 ]]; then
+        break
+    fi
+done
